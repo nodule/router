af6b4df9b85c5b1b9fe11643ad76c10482c19718
Rob Halff <rob.halff@gmail.com>
2014-12-09 22:53:01 +0100

    update

--- nodes/client/node.js
+++ nodes/client/node.js
@@ -1,13 +1,13 @@
-output = function() {
+on.input.routes = function(data) {
   var router = router_lib.router;
-  router.setMatchedPaths(input.routes);
+  router.setMatchedPaths(data);
   router.setOnChangeHandler(function(uri, route) {
     if (route) {
       var ret = {out: {}};
       ret.out[route.path] = route;
-      cb(ret);
+      output(ret);
     } else {
-      cb({ error: new Error('URI was not matched') });
+      output({ error: new Error('URI was not matched') });
     }
   });
 };
